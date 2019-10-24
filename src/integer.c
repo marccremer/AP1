4607e2b5ac510828cd8f848da2430678933ac167
Marc Cremer <marc@cremer.dev>
2019-10-25 01:23:36 +0200

    added sizeof

--- src/integer.c
+++ src/integer.c
@@ -37,16 +37,23 @@ int main()
 	printf("Die Summe von %i und %i ist: %i\n",arr[0],arr[1],result );
 	printf("Lets do some funky values\n");
 	printf("lets add large values\n");
-	int a,b;
+	int a,b,c;
 	a = 3276612231;
 	b = 3276612231;
-	printf("3276612231 + 3276612231 = %i\n",(a+b));
-	/*wir haben einen overflow und bekommen negative zahlen da wir denn speicherbereich von integer verlassen habben
-	Bei großen zahlen wird das Vorzeichen bit überschrieben und der cpu liest den speicher nun als negative zahl*/
+	c = a+b;
+	printf("3276612231 + 3276612231 = %i sizeof(%i)\n",c,sizeof(c));
+	/*wir haben einen overflow und bekommen negative zahlen da wir denn speicherbereich von integer verlassen haben.
+	Bei großen zahlen wird in den benachbarten speicher geschrieben ,aber nur 4 bytes gelesen(int) und die variable 
+	erscheint negative
+	c = 6553224462 in base 2  = (1) 10000110 10011010 01000101 00001110 ,wir haben mehr als 4 byte des integer
+	Die  (1) wird nicht mitgelesen->10000110 10011010 01000101 00001110=-2036710130 */
 	printf("lets add large negative values\n");
 	a = -3276612211;
 	b = -3276612211;
-	printf("-327661221 + -327661221 = %i\n",(a+b));	
-	/*hier warnte mich der compiler [-Woverflow] und das vorzeichen bit wurde wieder geflippt aber diesmal ins positive*/
+	c = a+b;
+	printf("-327661221 + -327661221 = %i sizeof(%i)\n",c,sizeof(c));	
+	/*hier warnte mich der compiler mit  [-Woverflow] und der Speicher läuft wieder über und liest nun die zahl als negative
+	c = -6553224422 in base 2 als 2-er Komplement = 11111111111111111111111111111110 01111001 01100101 10111011 00011010
+	Die (1..1) werden nicht mitgelesen -> 01111001 01100101 10111011 00011010 = 2036710170*/
 	return 0;
 }
